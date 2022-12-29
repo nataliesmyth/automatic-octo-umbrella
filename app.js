92266ec0929a6e882dd08e42889fd3b407a8a02d
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-28 22:34:01 -0500

    x marker functionality

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+const xClass = 'x';
+const oClass = 'o'
 const cellEls = document.querySelectorAll('[data-cell]');
+let oTurn;
 
 cellEls.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true }) // only fires a click event once
 });
 
 function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? oClass : xClass;
+    placeMark(cell, currentClass);
     // Place X or O
     // Check for win
     // Check for draw
     // Switch players
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
 }
