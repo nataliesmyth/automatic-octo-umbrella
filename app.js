dc85a8a3f8aac359925cf384282caf7f588087ca
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-28 23:29:30 -0500

    game switches from x to o after each click

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const xClass = 'x';
 const oClass = 'o'
 const cellEls = document.querySelectorAll('[data-cell]');
-let oTurn;
+const board = document.getElementById('board')
+let oTurn; // this will determine whose turn it is
 
-cellEls.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true }) // only fires a click event once
-});
+startGame();
+
+function startGame() {
+    oTurn = false;
+    cellEls.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true }) // only fires a click event once
+    });
+}
 
 function handleClick(e) {
     const cell = e.target;
@@ -15,8 +21,21 @@ function handleClick(e) {
     // Check for win
     // Check for draw
     // Switch players
+    switchTurn()
+    setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
+}
+
+function switchTurn() {
+    // Whenever this function is invoked, the turn will equal the opposite turn
+    oTurn = !oTurn
+};
+
+function setBoardHoverClass() {
+    board.classList.remove(xClass);
+    board.classList.remove(oClass);
+
 }
